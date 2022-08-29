c0501b23277596a580e32e6f69e3314af498e375
Krishna <1india.kng@gmail.com>
2022-08-30 00:17:59 +0530

    user login

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const User = require("../models/User");
 const router = require("express").Router();
 const bcrypt = require("bcrypt");
+const { validate } = require("../models/User");
 
 //REGISTER
 router.post("/register", async (req, res) => {
@@ -25,5 +26,30 @@ router.post("/register", async (req, res) => {
   }
 });
 
+//login
+
+router.post("/login",async(req,res)=>{
+    console.log(req.body)
+    try {
+        //find user
+        const user = await User.findOne({username: req.body.username});
+         
+        !user && res.status(400).json("Wrong username or password!") 
+        
+        //validate password
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+
+        !validPassword && res.status(400).json("Wrong username or password!")
+
+        res.status(200).json({
+            _id: user.id, 
+            username: user.username
+        })
+
+    } catch (error) {
+        res.status(500).json(error);
+        
+    }
+})
 
 module.exports = router;
